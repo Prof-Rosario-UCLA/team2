9b5098c71665c9b5b43ccc32fc1f891109534c17
ashleykcheng <ashleykcheng@gmail.com>
2025-05-29 00:28:59 -0700

    add redis for walkins

--- backend/routes/walkin.js
+++ backend/routes/walkin.js
@@ -5,14 +5,25 @@ import {
   insertWalkIn,
 } from "../utils/mongodb.js";
 import { WalkIn } from "../models/WalkIn.js";
+import { cacheResult, fetchFromCache } from '../utils/redis.js';
 
 const router = express.Router();
 
 // Get all walk-ins
 router.get("/", async (req, res) => {
   try {
-    console.log("Trying to GET all walk-ins");
+    console.log("Attempting to fetch walk-in data from cache");
+    const cachedData = await fetchFromCache('walkin');
+    if (cachedData){
+      console.log("Data found in cache");
+      return res.status(200).json(cachedData);
+    }
+    else{
+      console.log("Data not found in cache");
+    }
+
     const walkIns = await getAllWalkIns();
+    await cacheResult('walkin', walkIns, 300);
     res.json(walkIns);
   } catch (error) {
     console.error("Error in GET /walkins:", error);
@@ -73,6 +84,10 @@ router.post("/create", async (req, res) => {
     };
 
     const result = await insertWalkIn(newWalkIn);
+
+    const updatedAllWalkIns = await getAllReservations();
+    await cacheResult('walkin', updatedAllWalkIns, 300);
+    
     res.status(201).json(result);
   } catch (error) {
     console.error("Error in POST /walkins:", error);
