eea6e5b7460eb95c6255b9e69a5d16a3f91934b5
ashleykcheng <ashleykcheng@gmail.com>
2025-06-03 20:50:47 -0700

    fix config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
-const isProduction = true;
+const isProduction = true; 
 
-export const API_BASE_URL = isProduction
-  ? "http://team2.cs144.org:1919"
-  : "http://localhost:1919";
+export const API_BASE_URL = isProduction 
+  ? 'http://34.83.14.253:1919'
+  : 'http://localhost:1919'; 
+
+export const GOOGLE_CLIENT_ID = '499257456410-dk3qd2oeum2rdm3adh08p04o0ft0afms.apps.googleusercontent.com'; 
