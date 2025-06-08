575e2892d1c9700efb6b9e78177f9e70b3421783
ashleykcheng <ashleykcheng@gmail.com>
2025-06-08 00:34:48 -0700

    push

--- src/frontend-config.ts
+++ src/frontend-config.ts
@@ -1,7 +1,7 @@
 const isProduction = true;
 
 export const API_BASE_URL = isProduction
-  ? "http://34.118.239.65:1919"
+  ? "http://restaurantapp-backend-service:1919"
   : "http://localhost:1919";
 
 // dear TAs if you see this, i know this is bad practice, but i spent a long time
