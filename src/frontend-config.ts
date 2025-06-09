c9a2ac60f00d87497189760f44d7e83b0340c067
ashleykcheng <ashleykcheng@gmail.com>
2025-06-08 22:18:41 -0700

    fix deploy

--- src/frontend-config.ts
+++ src/frontend-config.ts
@@ -7,7 +7,4 @@ export const API_BASE_URL = env == "dev"
   ? "http://localhost:1919"
   : "";
 
-// dear TAs if you see this, i know this is bad practice, but i spent a long time
-// debugging why the kubernetes cluster couldn't recognize the github secret correctly
-// and i couldn't get it working correctly
-export const GOOGLE_CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID || "";
